2f7f19c01ad48c2f2d51e874ae72902e028c71a6
umairafatima29 <umairafatima897@gmail.com>
2025-12-06 10:16:32 +0500

    fixing errors

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -38,21 +38,21 @@ const AddAddress = () => {
       [name]: value,
     }));
   };
-
-  const onSubmitHandler = async (e) => {
-    e.preventDefault();
-    try {
-      const { data } = await axios.post("/api/address/add", { address });
-      if (data.success) {
-        toast.success(data.message);
-        navigate("/cart");
-      } else {
-        toast.error(data.message);
-      }
-    } catch (error) {
-      toast.error(error.message);
+const onSubmitHandler = async (e) => {
+  e.preventDefault();
+  try {
+  
+    const { data } = await axios.post("/api/address/add", address);
+    if (data.success) {
+      toast.success(data.message);
+      navigate("/cart");
+    } else {
+      toast.error(data.message);
     }
-  };
+  } catch (error) {
+    toast.error(error.message);
+  }
+};
 
   useEffect(() => {
     if (!user) {
